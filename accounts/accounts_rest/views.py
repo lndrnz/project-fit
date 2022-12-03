dd2616d54ae98ff8b0c4660aae655580882252dc
Lander Nunez <landernunez@gmail.com>
2022-12-03 13:45:40 -0600

    added more views.py stuff with put and delete

--- accounts/accounts_rest/views.py
+++ accounts/accounts_rest/views.py
@@ -59,3 +59,54 @@ def api_list_accounts(request):
             )
             response.status_code = 405
             return response
+
+def update_censors(request, pk):
+    if request.method == "PUT":
+        account = Account.objects.get(id=pk)
+        content = json.loads(request.body)
+        new_censor = content["censored"]
+
+        Account.objects.filter.get(id=pk)
+        return JsonResponse(
+            account,
+            encoder=AccountDetailEncoder,
+            safe=False
+        )
+    else:
+        return JsonResponse({"message": "Your request has failed"})
+
+@require_http_methods(["DELETE", "PUT", "GET"])
+def api_show_account(request,pk):
+    if request.method == "GET":
+        account = Account.objects.get(id=pk)
+        return JsonResponse (
+            account,
+            encoder=AccountDetailEncoder,
+            safe=False
+        )
+    elif request.method =="DELETE":
+        count, _= Account.objects.filter(id=pk).delete()
+        Account.objects.filter(id=pk).delete()
+        return JsonResponse ({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+        new_username=content["username"]
+        new_password = make_password(content["password"])
+        new_first_name=content["first_name"]
+        new_last_name=content["last_name"]
+        new_email=["email"]
+
+        Account.objects.filter(id=pk).update(
+            username=new_username,
+            password=new_password,
+            email=new_email,
+            first_name=new_first_name,
+            last_name=new_first_name
+        )
+
+        account = Account.objects.get(id=pk)
+        return JsonResponse (
+            account,
+            encoder=AccountDetailEncoder,
+            safe=False,
+        )
